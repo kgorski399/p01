d85067cbbadc97ab04c226e001bbe8c973a104af
kgorski399 <kgorski399@gmail.com>
2024-09-28 05:03:22 +0200

    debug

--- terraform/dev/lambda_test.tf
+++ terraform/dev/lambda_test.tf
@@ -31,9 +31,8 @@ resource "aws_lambda_function" "my_lambda" {
   function_name = "my_lambda_function"
   handler       = "lambda.lambda_handler"
   runtime       = "python3.12"
-
   role          = aws_iam_role.lambda_role.arn
-  filename      = "../lambdas/testlambda/lambda_function.zip"
+  filename      = "lambdas/testlambda/lambda_function.zip"
 
   # use hash from output GA variable
   source_code_hash = "${var.zip_hash}"
