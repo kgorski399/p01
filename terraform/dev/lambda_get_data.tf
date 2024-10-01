b38ae27da15d5b6adaf6e5e254a231f0a43f63f6
admin <kgorski399@gmail.com>
2024-10-01 18:36:55 +0200

    get data lambda

---
+++ terraform/dev/lambda_get_data.tf
@@ -0,0 +1,18 @@
+
+
+
+resource "aws_lambda_function" "get_data_lambda" {
+  function_name    = "get_data"
+  role             = aws_iam_role.lambda_role.arn
+  handler          = "lambda.lambda_handler"
+  source_code_hash = data.archive_file.get_data_code.output_base64sha256
+  runtime          = "python3.12"
+  filename         = data.archive_file.get_data_code.output_path
+}
+
+# Archive ZIP for the "get_data" Lambda function
+data "archive_file" "get_data_code" {
+  type        = "zip"
+  source_dir  = "${path.module}/../../lambdas/get_data"
+  output_path = "${path.module}/../../lambdas/get_data.zip"
+}
