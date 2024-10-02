ef9b902a044d098e0b28462bda8ba71b3a9c28bb
admin <kgorski399@gmail.com>
2024-10-02 02:15:10 +0200

    hotfix

--- terraform/dev/lambda_feed_or_water.tf
+++ terraform/dev/lambda_feed_or_water.tf
@@ -4,7 +4,7 @@
 resource "aws_lambda_function" "feed_or_water_function" {
   function_name    = "feed_or_water_function"
   role             = aws_iam_role.lambda_role.arn
-  handler          = "lambda.lambda_handler"
+  handler          = "feed_or_water.lambda_handler"
   source_code_hash = data.archive_file.feed_or_water_code.output_base64sha256
   runtime          = "python3.12"
   filename         = data.archive_file.feed_or_water_code.output_path
